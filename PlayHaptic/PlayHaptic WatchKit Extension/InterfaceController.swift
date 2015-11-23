fd24e185c8ce075b3b6c6c67e31468335d842e01
Maurice Kelly <osv-github@chatswood.org.uk>
2015-11-23 20:22:56 +0000

    Configure the picker in code and play haptics

--- PlayHaptic/PlayHaptic WatchKit Extension/InterfaceController.swift
+++ PlayHaptic/PlayHaptic WatchKit Extension/InterfaceController.swift
@@ -7,12 +7,31 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var hapticPicker: WKInterfacePicker!
 
     @IBAction func stylePicked(value: Int) {
-
+        let hapticStyle = styles[value].1
+        WKInterfaceDevice.currentDevice().playHaptic(hapticStyle)
     }
+
+    let styles:[(String, WKHapticType)] = [
+        ("Notification", .Notification),
+        ("DirectionUp", .DirectionUp),
+        ("DirectionDown", .DirectionDown),
+        ("Success", .Success),
+        ("Failure", .Failure),
+        ("Retry", .Retry),
+        ("Start", .Start),
+        ("Stop", .Stop),
+        ("Click", .Click)
+    ]
+
     override func awakeWithContext(context: AnyObject?) {
+        let pickerItems: [WKPickerItem] = styles.map { style -> WKPickerItem in
+            let pickerItem = WKPickerItem()
+            pickerItem.title = style.0
+            return pickerItem
+        }
+        hapticPicker.setItems(pickerItems)
+
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
 
     override func willActivate() {
