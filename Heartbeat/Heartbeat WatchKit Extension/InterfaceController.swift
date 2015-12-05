afc3b9fca08b4f753b991acb408fcade6b12e821
Mark Goody <mgoody@shopkeep.com>
2015-12-04 22:34:29 -0500

    Manage the state of the button and the label

--- Heartbeat/Heartbeat WatchKit Extension/InterfaceController.swift
+++ Heartbeat/Heartbeat WatchKit Extension/InterfaceController.swift
@@ -8,7 +8,40 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var label: WKInterfaceLabel!
     
     @IBAction func buttonTapped() {
+        if readingHeartRate {
+            endReadingHeartRate()
+        } else {
+            beginReadingHeartRate()
+        }
+    }
+    
+    private var readingHeartRate = false {
+        didSet {
+            updateButton()
+        }
+    }
+    
+    private func updateButton() {
+        if readingHeartRate {
+            button.setTitle("Stop")
+        } else {
+            button.setTitle("Start")
+        }
+    }
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
         
+        label.setText("-")
+        updateButton()
+    }
+    
+    private func beginReadingHeartRate() {
+        readingHeartRate = true
+    }
+    
+    private func endReadingHeartRate() {
+        readingHeartRate = false
     }
 
 }
