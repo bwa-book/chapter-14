07a7a71753aa0585efa7f22f26ebdfce1cf0f534
Mark Goody <mgoody@shopkeep.com>
2015-12-04 23:03:21 -0500

    Add a property for the CoreMotion manager

--- Swoosh/Swoosh WatchKit Extension/InterfaceController.swift
+++ Swoosh/Swoosh WatchKit Extension/InterfaceController.swift
@@ -8,5 +8,7 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var xLabel: WKInterfaceLabel!
     @IBOutlet var yLabel: WKInterfaceLabel!
     @IBOutlet var zLabel: WKInterfaceLabel!
+    
+    let manager = CMMotionManager()
 
 }
