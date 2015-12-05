c3ed868aa71e2857d647d0e1d310f00c7a017344
Mark Goody <mgoody@shopkeep.com>
2015-12-04 22:47:30 -0500

    Handle HealthKit authorization request

--- Heartbeat/Heartbeat/AppDelegate.swift
+++ Heartbeat/Heartbeat/AppDelegate.swift
@@ -1,9 +1,16 @@
 import UIKit
+import HealthKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func applicationShouldRequestHealthAuthorization(application: UIApplication) {
+        HKHealthStore().handleAuthorizationForExtensionWithCompletion { (success, error) in
+            
+        }
+    }
 
 }
 
