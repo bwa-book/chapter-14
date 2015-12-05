80f768a7b18c9af2c44bd5c58881d78bd6284751
Mark Goody <mgoody@shopkeep.com>
2015-12-04 23:02:55 -0500

    Add the labels

--- Swoosh/Swoosh WatchKit Extension/InterfaceController.swift
+++ Swoosh/Swoosh WatchKit Extension/InterfaceController.swift
@@ -1,9 +1,12 @@
 import WatchKit
 import Foundation
+import CoreMotion
 
 
 class InterfaceController: WKInterfaceController {
 
-    
+    @IBOutlet var xLabel: WKInterfaceLabel!
+    @IBOutlet var yLabel: WKInterfaceLabel!
+    @IBOutlet var zLabel: WKInterfaceLabel!
 
 }
