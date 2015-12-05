f9acfa2f5ebfd3d11e2782e44cc1f4c9362852d5
Mark Goody <mgoody@shopkeep.com>
2015-12-04 23:07:22 -0500

    Add willActivate method

--- Swoosh/Swoosh WatchKit Extension/InterfaceController.swift
+++ Swoosh/Swoosh WatchKit Extension/InterfaceController.swift
@@ -10,5 +10,27 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var zLabel: WKInterfaceLabel!
     
     let manager = CMMotionManager()
+    
+    override func willActivate() {
+        super.willActivate()
+        
+        guard manager.accelerometerAvailable else {
+            xLabel.setText("⚠️")
+            yLabel.setText("⚠️")
+            zLabel.setText("⚠️")
+            return
+        }
+        
+        manager.accelerometerUpdateInterval = 0.2
+        manager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!) { data, error in
+            if let data = data {
+                self.xLabel.setText("X: \(data.acceleration.x)")
+                self.yLabel.setText("Y: \(data.acceleration.y)")
+                self.zLabel.setText("Z: \(data.acceleration.z)")
+            } else {
+                print(error?.description)
+            }
+        }
+    }
 
 }
