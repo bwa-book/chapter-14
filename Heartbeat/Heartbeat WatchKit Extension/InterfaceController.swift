13e5773344cb3a664f068cfd28dab098c542e0c9
Mark Goody <mgoody@shopkeep.com>
2015-12-04 22:40:02 -0500

    Add error method

--- Heartbeat/Heartbeat WatchKit Extension/InterfaceController.swift
+++ Heartbeat/Heartbeat WatchKit Extension/InterfaceController.swift
@@ -52,6 +52,11 @@ class InterfaceController: WKInterfaceController {
             workoutSession?.delegate = self
         }
     }
+    
+    private func updateWithNoAccess() {
+        label.setText("⚠️")
+        endReadingHeartRate()
+    }
 
 }
 
